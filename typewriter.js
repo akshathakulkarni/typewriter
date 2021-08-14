71aed6b5be15a0a4dac98deb5f25b4ddcd9396c7
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-13 22:01:25 -0400

    Display sentence as an animation

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0; 
+for (let char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, delay);
+  delay += 50;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 1600);
+
+  
+  
